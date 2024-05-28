2b3f91f077e076999fa5437c51540743979be67b
Arpit <arpit.mishra.out@gmail.com>
2024-05-28 14:03:41 +0530

    Dynamiclly generate the product HTML

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 `use strict`;
+import { products } from "./products.js";
 
 // Getting Acess to all the dynamic elements
 
@@ -9,10 +10,80 @@ const showQty = document.querySelector(`.item-qty`);
 
 const optionProducts = document.querySelectorAll(`.choice-img`);
 
+const productImg = document.querySelector(`.product-img`);
+const productTopPart = document.querySelector(`.product-top-part`);
+
+// When Page Loads at start
+// *************************************
+// *************************************
+userChoosenProduct(optionProducts[0]);
+generateProduct(0);
+
+// **************************************
+// **************************************
+
+// Generating Dynamic HTML FOR the product
+
+function generateProduct(index) {
+  // State variables
+  let productHTML = ``;
+  // creating product image and inserting it into the dom
+  productImg.innerHTML = ``;
+  // productTopPart.innerHTML = ``;
+
+  const productImage = document.createElement(`img`);
+  productImage.src = `${products[index][`image`]}`;
+  productImage.classList.add(`img-product`);
+  productImage.setAttribute(`alt`, `Product-${index + 1} Image`);
+
+  productImg.append(productImage);
+
+  // ***************************
+  // ***************************
+  // Generating HTML For the Product Details
+  productHTML = `
+   <div class="company">
+          <p class="product-company">sneaker company</p>
+    </div>
+
+    <div class="product-title">
+      <h2>${products[index][`title`]}</h2>
+    </div>
+
+    <div class="product-description">
+        <p>
+            These low-profile sneakers are your perfect casual wear
+            companion. Featuring a durable rubber outer sole, they will
+            withstand everything the weather can offer.
+        </p>
+    </div>
+
+    <div class="product-pricing">
+        <p class="discounted-price">$${products[index][`discountPrice`].toFixed(
+          2
+        )}</p>
+        <p class="total-discount">
+        ${products[index][`percentage`]}%
+        </p>
+    </div>
+
+    <div class="actual-price">
+        $${products[index].actualPrice.toFixed(2)}
+    </div>
+
+  
+  `;
+
+  // Appending it to the dom
+  productTopPart.innerHTML = productHTML;
+}
+
 // ***********************************
+// ***********************************
+
 // adding feature of product choices to the the, so when any product got chosen it's styling got changed
 
-const userChoosenProduct = (choice) => {
+function userChoosenProduct(choice) {
   // Changing the style of the product
   optionProducts.forEach((optionChoice) => {
     if (optionChoice !== choice) {
@@ -21,10 +92,11 @@ const userChoosenProduct = (choice) => {
       optionChoice.classList.add(`active`);
     }
   });
-};
+}
 
-optionProducts.forEach((optionChoice) => {
+optionProducts.forEach((optionChoice, index) => {
   optionChoice.addEventListener(`click`, function (e) {
     userChoosenProduct(optionChoice);
+    generateProduct(index);
   });
 });
