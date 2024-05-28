06932f38ea795f2f7291e57fb2d7333cb4eb1722
Arpit <arpit.mishra.out@gmail.com>
2024-05-28 13:03:28 +0530

    Added product-Choice Feature to the sneakers-shopping-cart

--- js/script.js
+++ js/script.js
@@ -1,1 +1,30 @@
 `use strict`;
+
+// Getting Acess to all the dynamic elements
+
+const addCartBtn = document.getElementById(`add-Cart-Btn`);
+const increaseQtyBtn = document.getElementById(`plus-btn`);
+const decreaseQtyBtn = document.getElementById(`minus-btn`);
+const showQty = document.querySelector(`.item-qty`);
+
+const optionProducts = document.querySelectorAll(`.choice-img`);
+
+// ***********************************
+// adding feature of product choices to the the, so when any product got chosen it's styling got changed
+
+const userChoosenProduct = (choice) => {
+  // Changing the style of the product
+  optionProducts.forEach((optionChoice) => {
+    if (optionChoice !== choice) {
+      optionChoice.classList.remove(`active`);
+    } else {
+      optionChoice.classList.add(`active`);
+    }
+  });
+};
+
+optionProducts.forEach((optionChoice) => {
+  optionChoice.addEventListener(`click`, function (e) {
+    userChoosenProduct(optionChoice);
+  });
+});
